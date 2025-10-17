536d5673429f09e45adae16719ac327ae907a619
Ranyel Cerracena <rcerracena@gmail.com>
2025-10-17 17:58:58 -0300

    Implementação dos inputs do front end, junto do botão de calcular. Concluido a parte do front-end fora o consumo da api, que será implementado na proxima atualização do projeto.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,59 @@
 <script setup>
+import TurnBack from './components/TurnBack.vue';
 
 </script>
 
 <template>
     <form class="form">
+        <TurnBack/>
         <h1 class="title">Caluladora de Impressões</h1>
         <p class="text">Estime o custo dos seus projetos de impressão 3D com facilidade.</p>
 
         <section class="card">
             <div class="materialForm">
-                <div class="materialWeightInput">
-                    <label for="materialWeight"></label>
-                    <input type="text" name="materialWeight">
+                <div class="materialWeight">
+                    <label for="materialWeight">Peso do material</label>
+                    <input type="text" class="input-style" placeholder="Kg" name="materialWeight">
                 </div>
-                //https://drive.google.com/file/d/1fX5S4aeiUGQjj6bNSgKNjTogQ7MSJ74F/view?usp=drive_link
-                <input type="text">
+                <div class="materialPrice">
+                    <label for="materialPrice">Preço do material</label>
+                    <input type="text" class="input-style" placeholder="R$" name="materialPrice">
+                </div>
+            </div>
+            <div class="printerForm">
+                <div class="printerGrid">
+                    <div class="gridItem">
+                        <label for="printerModel">Material usado</label>
+                        <input id="printerModel" name="printerModel" class="input-style" placeholder="Gramas">
+                        <label for="printTime">Tempo de impressão</label>
+                        <input id="printTime" name="printTime" class="input-style" placeholder="Horas">
+                    </div>
+
+                    <div class="gridItem">
+                        <label for="printWeight">Consumo em Watts</label>
+                        <input id="printWeight" name="printWeight" class="input-style" placeholder="Watts">
+                        <label for="printerCostPerHour">Custo da energia</label>
+                        <input id="printerCostPerHour" name="printerCostPerHour" class="input-style" placeholder="kWh">
+                    </div>
+                </div>
+            </div>
+            <div class="profitForm">
+                    <label for="desiredProfit">Lucro desejado</label>
+                    <input id="desiredProfit" name="desiredProfit" class="input-style" placeholder="%">
+            </div>
+            <button class="calculateButton">
+                Calcular
+            </button>
+            <div class="response input-style">
+                <p class="totalWithoutProfit">Total sem Lucro: R$ xx,xx</p>
+                <p class="totalWithProfit">Total com Lucro: R$ xx,xx</p>
             </div>
         </section>
     </form>
 </template>
 
 <style scoped>
-.form{
+.form {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -29,33 +61,86 @@
     width: 100vw;
     background-color: #f5f5f5;
 }
-
-
-.text, .title{
+.text,
+.title {
     color: #333;
     margin: 0;
 }
 
-.text{
+.text {
     margin-bottom: 20px;
     display: inline-block;
 }
 
-.title{
+.title {
     text-align: center;
     margin-top: 20px;
     font-size: 2rem;
 }
-.card{
+
+.card {
     box-shadow: 0 0px 8px rgba(0, 0, 0, 0.1);
     border-radius: 5px;
-    width:  90%;
+    width: auto;
     height: 70dvh;
 }
-.materialForm{
+
+.materialForm {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 20px;
+}
+
+.materialWeight,
+.materialPrice {
     display: flex;
     flex-direction: column;
-    gap: 20px;
-    padding: 20px;
+    gap: 10px;
+}
+
+label,
+p {
+    color: #555;
+}
+
+.input-style {
+    padding: 10px;
+    border: 2px solid #ccc;
+    border-radius: 5px;
+    background-color: #f3f3f3;
+    font-size: 16px;
+    color: #555;
+    outline: none;
+}
+
+.input-style:focus {
+    border-color: #c164ffcb;
+    box-shadow: 0 0 0 0.2rem #d400ff2d;
+}
+.printerGrid{
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+}
+.gridItem{
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin: 10px;
+}
+.profitForm{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.calculateButton{
+    margin-top: 50px;
+    width: 97%;
+    border-radius: 5px;
+}
+.response{
+    margin-top: 50px;
+    text-align: center;
 }
 </style>
---
+++ frontend/src/components/TurnBack.vue
@@ -0,0 +1,32 @@
+<template>
+    <a class="buttonTurnBack" href="https://www.nerdstecnologias.com.br">
+        <i class="bi bi-arrow-left"></i>
+    </a>
+</template>
+
+<script setup>
+defineOptions({
+    name: 'TurnBack'
+});
+</script>
+
+<style scoped>
+.buttonTurnBack {
+    position: absolute;
+    top: 30px;
+    left: 30px;
+    z-index: 2;
+    border: 2px solid #4e0b7e;
+    width: 70px;
+    height: 35px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+    transition: background-color 0.25s ease-in-out, color 0.25s ease-in-out;
+}
+.buttonTurnBack:hover{
+    background-color: #4e0b7e;
+    color: #ffffff;
+}
+</style>
